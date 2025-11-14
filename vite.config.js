f56737f7d7d94600a0b6c88e8d5374e659fad152
AlexRieger47 <alexrieger.147@gmail.com>
2025-11-14 18:01:35 -0300

    [FIX] Arreglos para funcionalidad en GH-Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mira-gametracker",
+  base: "/mira-gametracker/",
   server: {
     port: 3000,
     proxy: {

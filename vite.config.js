1067fdc6ce719896d470bba7f78a1078daa272c1
AlexRieger47 <alexrieger.147@gmail.com>
2025-11-14 18:19:28 -0300

    [FIX] vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://gametracker-api-y3q1.onrender.com',
+        target: 'https://gametracker-api-y3q1.onrender.com/',
         changeOrigin: true,
         secure: false
       }

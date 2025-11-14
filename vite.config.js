3bd15333d322cbe64651cd726401dbfafa422261
AlexRieger47 <alexrieger.147@gmail.com>
2025-11-14 18:19:56 -0300

    [FIX] vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mira-gametracker",
+  base: "/mira-gametracker/",
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://gametracker-api-y3q1.onrender.com/',
+        target: 'https://gametracker-api-y3q1.onrender.com',
         changeOrigin: true,
         secure: false
       }
